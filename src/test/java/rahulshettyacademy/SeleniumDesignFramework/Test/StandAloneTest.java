244d84fe24e500005b6021703e89dfed6ad2478d
forampatel5220 <patelforam133@gmail.com>
2024-04-07 15:13:33 -0400

    updated

--- src/test/java/rahulshettyacademy/SeleniumDesignFramework/Test/StandAloneTest.java
+++ src/test/java/rahulshettyacademy/SeleniumDesignFramework/Test/StandAloneTest.java
@@ -23,6 +23,7 @@ public class StandAloneTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
                 //new comments are added
+                //for testing
 	String productname= "ZARA COAT 3";	
 	WebDriverManager.chromedriver().setup();
 	WebDriver driver=new ChromeDriver();
